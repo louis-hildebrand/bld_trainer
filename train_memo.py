36dded58b7a72f8007ddf3dc6d70bc3a9d02081c
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-08-29 11:17:57 -0400

    Catch KeyboardInterrupts

--- train_memo.py
+++ train_memo.py
@@ -26,45 +26,49 @@ def _human_readable_time(t: timedelta) -> str:
     return f"{m:d}:{s:02d}.{ms:03d}"
 
 
+def _do_solve() -> None:
+    clear_screen()
+    # Generate random scramble and print scramble sequence
+    scramble = RubiksCubeScrambler.random_scramble()
+    print(f"Scramble: {' '.join([str(m) for m in scramble])}")
+    rc = RubiksCube().apply(scramble)
+    print("\n" + RubiksCubeDrawer.draw(rc) + "\n")
+    # Input solution
+    input("Press ENTER to start")
+    print()
+    start_utc = datetime.utcnow()
+    start = time.time()
+    corner_targets = _input_targets(f"Corners:\n# ")
+    corner_end_time = time.time()
+    edge_targets = _input_targets(f"Edges:\n# ")
+    edge_end_time = time.time()
+    # Check solution
+    rc = rc.apply([Move.Z2])
+    rc = M2Solver.apply_solution(rc, edge_targets, corner_targets)
+    print("\n" + RubiksCubeDrawer.draw(rc) + "\n")
+    # Save and print stats
+    success = rc.is_solved()
+    corners_duration = timedelta(seconds = corner_end_time - start)
+    edges_duration = timedelta(seconds = edge_end_time - corner_end_time)
+    save_result(
+        start_utc, scramble, corners_duration, edges_duration, corner_targets, edge_targets, success
+    )
+    print("Memorization successful!" if success else "Memorization failed.")
+    print(f"Corners: {_human_readable_time(corners_duration)}")
+    print(f"Edges:   {_human_readable_time(edges_duration)}")
+    print(f"Total:   {_human_readable_time(corners_duration + edges_duration)}")
+    print()
+    input("Press ENTER to continue")
+    time.sleep(0.1)
+
+
 def main():
-    while True:
-        clear_screen()
-        # Generate random scramble and print scramble sequence
-        scramble = RubiksCubeScrambler.random_scramble()
-        print(f"Scramble: {' '.join([str(m) for m in scramble])}")
-        rc = RubiksCube().apply(scramble)
+    try:
+        while True:
+            _do_solve()
+    except KeyboardInterrupt:
         print()
-        print(RubiksCubeDrawer.draw(rc))
-        print()
-        # Input solution
-        input("Press ENTER to start")
-        print()
-        start_utc = datetime.utcnow()
-        start = time.time()
-        corner_targets = _input_targets(f"Corners:\n# ")
-        corner_end_time = time.time()
-        edge_targets = _input_targets(f"Edges:\n# ")
-        edge_end_time = time.time()
-        # Check solution
-        rc = rc.apply([Move.Z2])
-        rc = M2Solver.apply_solution(rc, edge_targets, corner_targets)
-        print()
-        print(RubiksCubeDrawer.draw(rc))
-        print()
-        # Save and print stats
-        success = rc.is_solved()
-        corners_duration = timedelta(seconds = corner_end_time - start)
-        edges_duration = timedelta(seconds = edge_end_time - corner_end_time)
-        save_result(
-            start_utc, scramble, corners_duration, edges_duration, corner_targets, edge_targets, success
-        )
-        print("Memorization successful!" if success else "Memorization failed.")
-        print(f"Corners: {_human_readable_time(corners_duration)}")
-        print(f"Edges:   {_human_readable_time(edges_duration)}")
-        print(f"Total:   {_human_readable_time(corners_duration + edges_duration)}")
-        print()
-        input("Press ENTER to continue")
-        time.sleep(0.1)
+        print("Exiting...")
 
 
 if __name__ == "__main__":
